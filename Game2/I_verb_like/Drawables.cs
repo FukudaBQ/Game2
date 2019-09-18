0fa8d5676d44b566969bc9fe43d18004f3c65478
FukudaBQ <42679702+FukudaBQ@users.noreply.github.com>
2019-09-18 16:56:32 -0400

    Add verb like interface

---
+++ Game2/I_verb_like/Drawables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2.I_verb_like
+{
+    interface Drawables
+    {
+    }
+}
