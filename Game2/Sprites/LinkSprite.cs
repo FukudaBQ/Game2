cb16703afbb25adb5a555fb73a677c2ec48d5110
Zijie <Zijie@DESKTOP-920RLAV>
2019-09-20 21:03:04 -0400

    aaa

---
+++ Game2/Sprites/LinkSprite.cs
@@ -0,0 +1,16 @@
+﻿using Game2.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2.Sprites
+{
+    class LinkSprite : ISprite
+    {
+
+    }
+}
