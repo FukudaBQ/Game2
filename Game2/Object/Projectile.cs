4b26b337a52837f5ac29e6b55414a2cfbb1a18fe
Hangyu Ying <ying.121@buckeyemail.osu.edu>
2019-09-23 21:14:47 -0400

    part of projectile

---
+++ Game2/Object/Projectile.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Coleections.Generic;
+using Sysytem.Linq;
+using Sysyem.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game2.Sprites.Link
+{
+    class Projectile
+    {
+        private Vector2 position;
+        private int speed = 800;
+        private int radius = 15;
+        private Dir direction;
+
+        public static List<Projectile> projectiles = new List<>();
+
+        public Projectile(Vector2 newPos, Dir newDir)
+        {
+            position = newPos;
+            direction = newDir;
+        }
+
+        public Vector2 Positition
+        {
+            get { return position; }
+        }
+        public int Radius
+        {
+            get { return radius; }
+        }
+        public void Update(GameTime gameTime)
+        {
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            switch (direction)
+            {
+                case Dir.Right:
+                    position.X += speed * dt;
+                    break;
+                case Dir.Left:
+                    position.X -= speed * dt;
+                    break;
+                case Dir.Up:
+                    position.Y -= speed * dt;
+                    break;
+                case Dir.Down:
+                    position.Y += speed * dt;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+}
