e57fd26e1bc77b8f848d8d04584ae3d1ab1b6ea5
Zijie <Zijie@DESKTOP-920RLAV>
2019-09-25 17:59:54 -0400

    aaa

--- Game2/Game1.cs
+++ Game2/Game1.cs
@@ -50,6 +50,9 @@ namespace Game2
         Compass compass;
         HeartContainer heartContainer;
         Map map;
+        Bow bow;
+        Sword sword;
+        Arrow arrow;
         //Link player;
         //private static ContentManager myContent;
         //Zijie Wei
@@ -117,11 +120,14 @@ namespace Game2
             fairy = new Fairy(new Vector2(200, 130), spriteBatch);
             dragon = new Dragon(boss, new Vector2(800, 800), spriteBatch);
             oldMan = new OldMan(NPC, new Vector2(270, 110), spriteBatch);
-            heartContainer = new HeartContainer(item, new Vector2(370, 130),spriteBatch);
+            heartContainer = new HeartContainer(item, new Vector2(370, 140),spriteBatch);
             clock = new Clock(item, new Vector2(460, 135), spriteBatch);
             key = new Key(item, new Vector2(550, 135), spriteBatch);
             compass= new Compass(item, new Vector2(640, 135), spriteBatch);
             map = new Map(item, new Vector2(730, 50), spriteBatch);
+            bow = new Bow(item, new Vector2(820, 130), spriteBatch);
+            sword = new Sword(item, new Vector2(910, 40), spriteBatch);
+            arrow = new Arrow(item, new Vector2(955, 130), spriteBatch);
             Blocks.blocks.Add(new GeneralBlock(new Vector2(480, 392)));
             Blocks.blocks.Add(new GeneralBlock(new Vector2(480, 589)));
             Blocks.blocks.Add(new GeneralBlock(new Vector2(1320, 392)));
@@ -207,6 +213,9 @@ namespace Game2
             key.Draw();
             compass.Draw();
             map.Draw();
+            bow.Draw();
+            arrow.Draw();
+            sword.Draw();
             foreach (Projectile i in Projectile.bomb)
             {
                 spriteBatch.Draw(bomb, i.Position, Color.White);
---
+++ Game2/Sprites/Items/Bow.cs
@@ -0,0 +1,34 @@
+﻿using Game2.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2.Sprites.Items
+{
+    class Bow : ISprite
+    {
+        public Texture2D texture { get; set; }
+        public Vector2 location { get; set; }
+        public SpriteBatch spriteBatch { get; set; }
+        public Bow(Texture2D texture, Vector2 location, SpriteBatch batch)
+        {
+            this.texture = texture;
+            this.location = location;
+            spriteBatch = batch;
+        }
+        public void Draw()
+        {
+            Rectangle sourceRectangle = new Rectangle(320, 0,13, 40);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, 13 * 4, 40 * 4);
+            spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+
+        public void Update(GameTime gametime)
+        {
+        }
+    }
+}
