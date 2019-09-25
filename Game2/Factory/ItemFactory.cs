abe6fc606f6396483a06e9db332754f0a3262cd9
FukudaBQ <42679702+FukudaBQ@users.noreply.github.com>
2019-09-25 11:34:51 -0400

    temp commit to pull

---
+++ Game2/Factory/ItemFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2.Factory
+{
+    class ItemFactory
+    {
+        public static ItemFactory instance = new ItemFactory();
+        public static ItemFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    private ItemFactory()
+    {
+    }
+
+    }
+}
