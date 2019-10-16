4e70051d10f858be0e20b2ed227cfcec32203712
Hangyu Ying <ying.121@buckeyemail.osu.edu>
2019-10-16 12:39:16 -0400

    made fairy under Item class, implemented CollisionHandler class, which interacts with Item class

--- Game2/Collision/CollisionHandler.cs
+++ Game2/Collision/CollisionHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Game2.Object.Items;
+using Game2.Sprites.Link;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,23 @@ namespace Game2.Collision
 {
     class CollisionHandler
     {
+        public void CollisionHandle(Player player)
+        {
+            foreach(Item it in Item.items)
+            {
+                if (it.GetType() == typeof(Fairy2))
+                {
+                    
+                        int sum = player.radius + it.Radius;
+                        if (Vector2.Distance(player.Position, it.Position) < sum)
+                        {
+                            it.Collided = true;
+                        }
+                }
+
+            }
+            Item.items.RemoveAll(p => p.Collided);
+        }
+
     }
 }
